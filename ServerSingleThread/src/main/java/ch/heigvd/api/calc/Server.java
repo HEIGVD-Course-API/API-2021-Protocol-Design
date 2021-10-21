9cfecddea2d4b418f3e1622194effa6f468b1c06
b0ogieMan <anthony.coke@heig-vd.ch>
2021-10-21 17:27:51 +0200

    feat: add & mul support

--- ServerSingleThread/src/main/java/ch/heigvd/api/calc/Server.java
+++ ServerSingleThread/src/main/java/ch/heigvd/api/calc/Server.java
@@ -1,5 +1,6 @@
 package ch.heigvd.api.calc;
 
+import javax.naming.directory.InvalidAttributeValueException;
 import java.io.*;
 import java.net.ServerSocket;
 import java.net.Socket;
@@ -14,6 +15,7 @@ public class Server {
 
     private final static Logger LOG = Logger.getLogger(Server.class.getName());
     private final int PORT = 4242;
+    private final String RESULT = "RESULT IS ";
     /**
      * Main function to start the server
      */
@@ -36,9 +38,6 @@ public class Server {
 
         ServerSocket serverSocket;
         Socket clientSocket = null;
-        //BufferedWriter stdout = null;
-        BufferedReader stdin = null;
-        PrintWriter stdout = null;
 
         try {
             serverSocket = new ServerSocket(PORT);
@@ -52,32 +51,10 @@ public class Server {
             try{
                 LOG.log(Level.INFO, "waiting for new client");
                 clientSocket = serverSocket.accept();
-
                 if(clientSocket.isClosed()) {
                     System.out.println("client socket is closed");
                 }
-
-                stdin = new BufferedReader(
-                        new InputStreamReader(
-                                clientSocket.getInputStream(),
-                                StandardCharsets.UTF_8));
-                stdout =
-                        new PrintWriter(clientSocket.getOutputStream());
-
-                //String welcome = new String("WELCOME\n");
-                //stdout.write(welcome, 0, welcome.length());
-                //stdout.flush();
-                stdout.write('W');
-                stdout.flush();
-                System.out.println("Message sent");
-
-
-                LOG.info("Cleaning up resources...");
-                clientSocket.close();
-                stdin.close();
-                stdout.close();
-
-                //handleClient(clientSocket);
+                handleClient(clientSocket);
             } catch (IOException e) {
                 LOG.log(Level.SEVERE, e.toString(), e);
             } finally { // Is finally necessary ? Everything is in catch in the
@@ -87,16 +64,8 @@ public class Server {
                 } catch (IOException e) {
                     LOG.log(Level.SEVERE, e.toString(), e);
                 }
-                try {
-                    if(stdin != null) stdin.close();
-                } catch (IOException e) {
-                    LOG.log(Level.SEVERE, e.toString(), e);
-                }
-                if(stdout != null) stdout.close();
             }
         }
-
-
     }
 
     /**
@@ -115,6 +84,78 @@ public class Server {
          *     - Handle the message
          *     - Send to result to the client
          */
+        BufferedReader in = null;
+        BufferedWriter out = null;
 
+        try {
+            in = new BufferedReader(new InputStreamReader(clientSocket.getInputStream(), StandardCharsets.UTF_8));
+            out = new BufferedWriter(new OutputStreamWriter(clientSocket.getOutputStream(), StandardCharsets.UTF_8));
+
+
+            String welcome = "WELCOME \nSYNTAX: REQUEST {OP} {V1} {V2} \n" +
+                    "AVAILABLE_OPS \nADD V1 V2 \nMUL V1 V2 \nEND_OF_OPS \n";
+            out.write(welcome);
+            out.flush();
+            System.out.println("Message sent");
+
+
+            String line;
+            while ( (line = in.readLine()) != null ) {
+                if (line.equalsIgnoreCase("END")) {
+                    out.write("END_OF_COMMUNICATION...BYE\n");
+                    out.flush();
+                    break;
+                }
+                String[] parts = line.split(" ");
+                String result = "";
+                switch (parts[0]) {
+                    case "ADD":
+                        try {
+                            result = add(Integer.parseInt(parts[1]),
+                                    Integer.parseInt(parts[2]));
+                        } catch (InvalidAttributeValueException e) {
+                            LOG.log(Level.SEVERE, e.toString(), e);
+                        }
+                        break;
+                    case "MUL":
+                        try {
+                            result = multiply(Integer.parseInt(parts[1]),
+                                    Integer.parseInt(parts[2]));
+                        } catch (InvalidAttributeValueException e) {
+                            LOG.log(Level.SEVERE, e.toString(), e);
+                        }
+                        break;
+                }
+                out.write(result);
+                out.flush();
+            }
+
+            LOG.info("Cleaning up resources...");
+            clientSocket.close();
+            in.close();
+            out.close();
+
+        } catch (IOException e) {
+            LOG.log(Level.SEVERE, e.toString(), e);
+        } finally {
+            try {
+                if(in != null) in.close();
+            } catch (IOException e) {
+                LOG.log(Level.SEVERE, e.toString(), e);
+            }
+            try {
+                if(out != null) out.close();
+            } catch (IOException e) {
+                LOG.log(Level.SEVERE, e.toString(), e);
+            }
+        }
+    }
+
+    private String add(int v1, int v2) throws InvalidAttributeValueException {
+        return RESULT + (v1 + v2) + '\n';
+    }
+
+    private String multiply(int v1, int v2) throws InvalidAttributeValueException {
+        return RESULT + (v1 * v2) + '\n';
     }
 }
