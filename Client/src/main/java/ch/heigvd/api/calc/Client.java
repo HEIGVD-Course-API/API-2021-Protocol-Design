1e87c1735e61d0d10ca1c2110f5c6eb8f3959ef1
b0ogieMan <anthony.coke@heig-vd.ch>
2021-10-21 17:43:21 +0200

    fix: receives and sends messages

--- Client/src/main/java/ch/heigvd/api/calc/Client.java
+++ Client/src/main/java/ch/heigvd/api/calc/Client.java
@@ -1,6 +1,7 @@
 package ch.heigvd.api.calc;
 
 import java.io.*;
+import java.net.InetAddress;
 import java.net.Socket;
 import java.util.logging.Level;
 import java.util.logging.Logger;
@@ -11,7 +12,9 @@ import java.util.logging.Logger;
 public class Client {
 
     private static final Logger LOG = Logger.getLogger(Client.class.getName());
-    private static int PORT = 4242;
+    private static final int PORT = 4242;
+    private static final String HOST = "localhost";
+
     /**
      * Main function to run client
      *
@@ -21,9 +24,10 @@ public class Client {
         // Log output on a single line
         System.setProperty("java.util.logging.SimpleFormatter.format", "%4$s: %5$s%6$s%n");
 
-        BufferedReader stdin = null;
-        BufferedWriter stdout = null;
         Socket clientSocket = null;
+        BufferedWriter out = null;
+        BufferedReader in = null;
+
 
         /* TODO: Implement the client here, according to your specification
          *   The client has to do the following:
@@ -35,55 +39,32 @@ public class Client {
          *     - read the response line from the server (using BufferedReader.readLine)
          */
         try {
-            System.out.println("Client: Initiating connection on " + PORT + "...");
-
-            clientSocket = new Socket("localhost", PORT);
-
-            if (clientSocket.isClosed()) {
-                System.out.println("Connection is closed. Aborting...");
-                return;
-            }
-
+            clientSocket = new Socket(HOST, PORT);
             System.out.println(clientSocket.getInetAddress());
-            stdin = new BufferedReader(new InputStreamReader(System.in));
-            stdout = new BufferedWriter(new OutputStreamWriter(System.out));
+            out = new BufferedWriter(new OutputStreamWriter(clientSocket.getOutputStream()));
+            in = new BufferedReader(new InputStreamReader(clientSocket.getInputStream()));
 
             String line;
-
-            while((line = stdin.readLine()) != null) {
-                stdout.write(line);
-
-                if (line.startsWith("QUIT"))
-                    break;
-
-                LOG.log(Level.INFO, "Server: " + stdin.readLine());
+            while ((line = in.readLine()) != null) {
+                System.out.println(line);
             }
 
-            LOG.log(Level.INFO, "Client: QUIT command sent, quitting...");
-
-        }
-        catch (IOException e){
+        } catch (IOException e){
             LOG.log(Level.SEVERE, e.toString(), e);
-        }
-        finally {
+        } finally {
             try {
-                if (stdin != null) stdin.close();
-            }
-            catch (IOException e) {
+                if(in != null) in.close();
+            } catch (IOException e) {
                 LOG.log(Level.SEVERE, e.toString(), e);
             }
-
             try {
-                if (stdout != null) stdout.close();
-            }
-            catch (IOException e) {
+                if(out != null) out.close();
+            } catch (IOException e) {
                 LOG.log(Level.SEVERE, e.toString(), e);
             }
-
             try {
-                if (clientSocket != null && !clientSocket.isClosed()) clientSocket.close();
-            }
-            catch (IOException e) {
+                if(clientSocket != null && !clientSocket.isClosed()) clientSocket.close();
+            } catch (IOException e) {
                 LOG.log(Level.SEVERE, e.toString(), e);
             }
         }
