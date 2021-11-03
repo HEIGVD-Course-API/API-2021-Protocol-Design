07e7a3d6234208fc119518539c5d9f271bea327e
chloefont <cfontaine@citycable.ch>
2021-11-03 11:37:45 +0100

    Single thread server start

--- ServerSingleThread/src/main/java/ch/heigvd/api/calc/Server.java
+++ ServerSingleThread/src/main/java/ch/heigvd/api/calc/Server.java
@@ -3,6 +3,7 @@ package ch.heigvd.api.calc;
 import java.io.*;
 import java.net.ServerSocket;
 import java.net.Socket;
+import java.nio.charset.StandardCharsets;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 
@@ -12,6 +13,20 @@ import java.util.logging.Logger;
 public class Server {
 
     private final static Logger LOG = Logger.getLogger(Server.class.getName());
+    private final int LISTEN_PORT = 3000;
+
+    private static final String WELCOME_MESS = "Welcome.\n";
+    private static final String HELP_MESS = "HELP MENU\n" +
+            "Syntax : OP N1 N2\n" +
+            "Supported operations :\n" +
+            "- ADD\n" +
+            "- MULT\n" +
+            "To end the connexion, enter END\n" +
+            "To print the help menu, enter HELP\n";
+    private final String ENTER_CALCUL_MESS = "Enter your calcul.\n";
+
+    private final String OK_CODE = "[OK]";
+    private final String ERR_CODE = "[ERR]";
 
     /**
      * Main function to start the server
@@ -31,7 +46,34 @@ public class Server {
          *  The receptionist just creates a server socket and accepts new client connections.
          *  For a new client connection, the actual work is done by the handleClient method below.
          */
+        Socket clientSocket = null;
+        BufferedReader reader = null;
+        BufferedWriter writer = null;
 
+        LOG.info("Starting server...");
+        try {
+            ServerSocket serverSocket = new ServerSocket(LISTEN_PORT);
+
+            while (true) {
+                // Blocked until a connection is made
+                clientSocket = serverSocket.accept();
+
+                reader = new BufferedReader(new InputStreamReader(clientSocket.getInputStream(), StandardCharsets.UTF_8));
+                writer = new BufferedWriter(new OutputStreamWriter(clientSocket.getOutputStream(), StandardCharsets.UTF_8));
+
+                writer.write(WELCOME_MESS + HELP_MESS);
+
+                String line;
+                while (!(line = reader.readLine()).equals("END")) {
+                    writer.write(ENTER_CALCUL_MESS);
+
+                    LOG.log(Level.INFO, line);
+                }
+        }
+
+        } catch (IOException e) {
+            LOG.log(Level.SEVERE, e.getMessage());
+        }
     }
 
     /**
