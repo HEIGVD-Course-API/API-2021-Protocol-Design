fe2579910f19d6d9cddb7d8b592189c8d6586d44
Rausis Justin <justin.rausis@heig-vd.ch>
2021-11-03 19:24:34 +0100

    Client

--- Client/src/main/java/ch/heigvd/api/calc/Client.java
+++ Client/src/main/java/ch/heigvd/api/calc/Client.java
@@ -1,7 +1,7 @@
 package ch.heigvd.api.calc;
 
-import java.io.BufferedReader;
-import java.io.InputStreamReader;
+import java.io.*;
+import java.net.Socket;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 
@@ -11,6 +11,65 @@ import java.util.logging.Logger;
 public class Client {
 
     private static final Logger LOG = Logger.getLogger(Client.class.getName());
+    private static final String BIENVENUE = "BIENVENUE";
+    private static final String CALCUL = "CALCUL";
+    private static final String RESLUTAT = "RESULT";
+    private static final String ERREUR = "ERREUR";
+    private static final String FIN = "QUITTER";
+    private static final String IP = "192.168.43.22";
+    private static final int PORT = 1024;
+    private Socket sClient;
+
+
+
+    public Boolean initialiseConnection(){
+        Socket clientSocket = null;
+        BufferedWriter out = null;
+        BufferedReader in = null;
+
+        try {
+            clientSocket = new Socket(IP, PORT);
+            in = new BufferedReader(new InputStreamReader(clientSocket.getInputStream()));
+
+
+            String line;
+            Boolean hasBienvenue = false;
+            while ((line = in.readLine()) != null){
+                if(line.equals(BIENVENUE)){
+                    sClient = clientSocket;
+                    hasBienvenue = true;
+                    System.out.println("Connection avec le serveur initialisé");
+                } else {
+                    System.out.println(line);
+                }
+            }
+            if(!hasBienvenue){
+                System.out.println("Connection avec le serveur impossible");
+            }
+            return hasBienvenue;
+        } catch (IOException ex) {
+            System.out.println("Connection avec le serveur impossible");
+            return false;
+        }
+    }
+
+    public String sendRequest(String request){
+        BufferedWriter out = null;
+        BufferedReader in = null;
+        try{
+            LOG.log(Level.INFO, request);
+            out = new BufferedWriter(new OutputStreamWriter(sClient.getOutputStream()));
+            in = new BufferedReader(new InputStreamReader(sClient.getInputStream()));
+            out.write(request);
+            out.flush();
+            String line;
+            while((line = in.readLine()) != null);
+            return line;
+        } catch (IOException ex) {
+            LOG.log(Level.INFO, "SA MARCHE PO");
+        }
+        return "";
+    }
 
     /**
      * Main function to run client
@@ -34,6 +93,23 @@ public class Client {
          */
 
         stdin = new BufferedReader(new InputStreamReader(System.in));
+
+        Client client = new Client();
+
+        Boolean stop = false;
+        if(client.initialiseConnection()){
+            while(!stop){
+                try {
+                    String request = stdin.readLine();
+                    if(request.equals(FIN)) stop = true;
+                    System.out.println(client.sendRequest(request));
+                } catch(IOException ex){
+
+                }
+            }
+        }
+
+        //client.sendRequest("CALCUL 3 + 2");
 
     }
 }
