f1af51a6cb5d6955f3b8e683580e5a3a61472622
Valentin Kaelin <kalanehd@gmail.com>
2021-11-03 11:52:53 +0100

    feat: add ServerSide work

--- ServerMultiThread/src/main/java/ch/heigvd/api/calc/Server.java
+++ ServerMultiThread/src/main/java/ch/heigvd/api/calc/Server.java
@@ -10,29 +10,43 @@ import java.util.logging.Logger;
  * Calculator server implementation - multi-thread
  */
 public class Server {
-
+    
     private final static Logger LOG = Logger.getLogger(Server.class.getName());
-
+    
+    private final static int PORT = 4269;
+    
     /**
      * Main function to start the server
      */
     public static void main(String[] args) {
         // Log output on a single line
         System.setProperty("java.util.logging.SimpleFormatter.format", "%4$s: %5$s%6$s%n");
-
+        
         (new Server()).start();
     }
-
+    
     /**
      * Start the server on a listening socket.
      */
     private void start() {
-
-        /* TODO: implement the receptionist server here.
-         *  The receptionist just creates a server socket and accepts new client connections.
-         *  For a new client connection, the actual work is done in a new thread
-         *  by a new ServerWorker.
-         */
-
+        ServerSocket serverSocket;
+        
+        try {
+            serverSocket = new ServerSocket(PORT);
+        } catch (IOException ex) {
+            LOG.log(Level.SEVERE, null, ex);
+            return;
+        }
+        
+        while (true) {
+            LOG.log(Level.INFO, "Multi-threaded: Waiting for a new client on port {0}", PORT);
+            try {
+                Socket clientSocket = serverSocket.accept();
+                LOG.info("A new client has arrived. Starting a new thread and delegating work to a new servant...");
+                new Thread(new ServerWorker(clientSocket)).start();
+            } catch (IOException ex) {
+                Logger.getLogger(Server.class.getName()).log(Level.SEVERE, null, ex);
+            }
+        }
     }
 }
--- ServerMultiThread/src/main/java/ch/heigvd/api/calc/ServerWorker.java
+++ ServerMultiThread/src/main/java/ch/heigvd/api/calc/ServerWorker.java
@@ -2,7 +2,6 @@ package ch.heigvd.api.calc;
 
 import java.io.*;
 import java.net.Socket;
-import java.nio.charset.StandardCharsets;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 
@@ -10,9 +9,18 @@ import java.util.logging.Logger;
  * Calculator worker implementation
  */
 public class ServerWorker implements Runnable {
-
+    
     private final static Logger LOG = Logger.getLogger(ServerWorker.class.getName());
-
+    
+    private Socket clientSocket;
+    private BufferedReader in = null;
+    private PrintWriter out = null;
+    
+    private final static String[] OPERATIONS = {"ADD", "SUB", "MULT", "DIV"};
+    private final static String ERROR_MSG = "ERROR: Calculation impossible";
+    private final static String END_WELCOME_MSG = "ERROR: Calculation impossible";
+    private final static String EXIT_CMD = "QUIT";
+    
     /**
      * Instantiation of a new worker mapped to a socket
      *
@@ -21,29 +29,90 @@ public class ServerWorker implements Runnable {
     public ServerWorker(Socket clientSocket) {
         // Log output on a single line
         System.setProperty("java.util.logging.SimpleFormatter.format", "%4$s: %5$s%6$s%n");
-
-        /* TODO: prepare everything for the ServerWorker to run when the
-         *   server calls the ServerWorker.run method.
-         *   Don't call the ServerWorker.run method here. It has to be called from the Server.
-         */
-
+        
+        try {
+            this.clientSocket = clientSocket;
+            in = new BufferedReader(new InputStreamReader(clientSocket.getInputStream()));
+            out = new PrintWriter(clientSocket.getOutputStream());
+        } catch (IOException ex) {
+            Logger.getLogger(ServerWorker.class.getName()).log(Level.SEVERE, null, ex);
+        }
     }
-
+    
     /**
      * Run method of the thread.
      */
     @Override
     public void run() {
-
-        /* TODO: implement the handling of a client connection according to the specification.
-         *   The server has to do the following:
-         *   - initialize the dialog according to the specification (for example send the list
-         *     of possible commands)
-         *   - In a loop:
-         *     - Read a message from the input stream (using BufferedReader.readLine)
-         *     - Handle the message
-         *     - Send to result to the client
-         */
-
+        String line;
+        boolean shouldRun = true;
+        
+        out.println("Welcome! Here are the available operations: (enter " + EXIT_CMD + " to leave)");
+        for (String op : OPERATIONS) {
+            out.println(op);
+        }
+        out.println(END_WELCOME_MSG);
+        
+        out.flush();
+        try {
+            LOG.info("Reading until client sends QUIT or closes the connection...");
+            while (shouldRun && (line = in.readLine()) != null) {
+                if (line.equalsIgnoreCase("quit")) {
+                    shouldRun = false;
+                    out.flush();
+                    continue;
+                }
+                String[] args = line.split(" ");
+                if (args.length != 3) {
+                    out.println(ERROR_MSG);
+                } else {
+                    String op = args[0];
+                    int n1 = Integer.parseInt(args[1]);
+                    int n2 = Integer.parseInt(args[2]);
+                    switch (op) {
+                        case "ADD":
+                            out.println(n1 + n2);
+                            break;
+                        case "SUB":
+                            out.println(n1 - n2);
+                            break;
+                        case "MULT":
+                            out.println(n1 * n2);
+                            break;
+                        case "DIV":
+                            out.println(n2 == 0 ? ERROR_MSG : n1 / n2);
+                            break;
+                        default:
+                            out.println(ERROR_MSG);
+                    }
+                }
+                out.flush();
+            }
+            
+            LOG.info("Cleaning up resources...");
+            clientSocket.close();
+            in.close();
+            out.close();
+            
+        } catch (IOException ex) {
+            if (in != null) {
+                try {
+                    in.close();
+                } catch (IOException ex1) {
+                    LOG.log(Level.SEVERE, "In BufferedReader cannot be closed");
+                }
+            }
+            if (out != null) {
+                out.close();
+            }
+            if (clientSocket != null) {
+                try {
+                    clientSocket.close();
+                } catch (IOException ex1) {
+                    LOG.log(Level.SEVERE, "ClientSocket cannot be closed");
+                }
+            }
+            LOG.log(Level.SEVERE, "Global error: client made a hard disconnect");
+        }
     }
 }
