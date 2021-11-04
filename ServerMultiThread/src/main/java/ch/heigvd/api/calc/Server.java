7bfae2d4ab41f45873c12fcffdce11f2c5a5d37b
Luhrel <lucien.perregaux@gmail.com>
2021-11-04 11:52:32 +0100

    Add welcome message.

--- ServerMultiThread/src/main/java/ch/heigvd/api/calc/Server.java
+++ ServerMultiThread/src/main/java/ch/heigvd/api/calc/Server.java
@@ -6,55 +6,38 @@ import java.net.Socket;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 
-/**
- * Calculator server implementation - multi-thread
- */
+/** Calculator server implementation - multi-thread */
 public class Server {
-    
-    private final int PORT = 3003;
 
-    private final static Logger LOG = Logger.getLogger(Server.class.getName());
+  private final int PORT = 3003;
 
-    /**
-     * Main function to start the server
-     */
-    public static void main(String[] args) {
-        // Log output on a single line
-        System.setProperty("java.util.logging.SimpleFormatter.format", "%4$s: %5$s%6$s%n");
+  private static final Logger LOG = Logger.getLogger(Server.class.getName());
 
-        (new Server()).start();
+  /** Main function to start the server */
+  public static void main(String[] args) {
+    // Log output on a single line
+    System.setProperty("java.util.logging.SimpleFormatter.format", "%4$s: %5$s%6$s%n");
+
+    (new Server()).start();
+  }
+
+  /** Start the server on a listening socket. */
+  private void start() {
+    try {
+      // Create the server socket
+      ServerSocket serverSocket = new ServerSocket(PORT);
+
+      // Waiting for new clients to connect
+      while (true) {
+        Socket clientSocket = serverSocket.accept();
+
+        // Create new thread to handle this client
+        ServerWorker worker = new ServerWorker(clientSocket); // create worker
+        Thread thread = new Thread(worker); // create thread with worker
+        thread.start(); // start thread
+      }
+    } catch (IOException e) {
+      System.out.println(e.getMessage());
     }
-
-    /**
-     * Start the server on a listening socket.
-     */
-    private void start() {
-
-        /* TODO: implement the receptionist server here.
-         *  The receptionist just creates a server socket and accepts new client connections.
-         *  For a new client connection, the actual work is done in a new thread
-         *  by a new ServerWorker.
-         */
-        
-        ServerSocket serverSocket = null;
-        try {
-            // Create the server socket
-            serverSocket = new ServerSocket(PORT);
-            
-            // Waiting for new clients to connect
-            while (true) {
-                Socket clientSocket = serverSocket.accept();
-
-                // Create new thread to handle this client
-                ServerWorker worker = new ServerWorker(clientSocket); // create worker
-                Thread thread = new Thread(worker); // create thread with worker
-                thread.start(); // start thread
-                thread.run();
-            }
-        }
-        catch (IOException e){
-            System.out.println(e.getMessage());
-        }
-
-    }
+  }
 }
--- ServerMultiThread/src/main/java/ch/heigvd/api/calc/ServerWorker.java
+++ ServerMultiThread/src/main/java/ch/heigvd/api/calc/ServerWorker.java
@@ -6,118 +6,115 @@ import java.nio.charset.StandardCharsets;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 
-/**
- * Calculator worker implementation
- */
+/** Calculator worker implementation */
 public class ServerWorker implements Runnable {
 
-    private final static Logger LOG = Logger.getLogger(ServerWorker.class.getName());
+  private static final Logger LOG = Logger.getLogger(ServerWorker.class.getName());
 
-    private Socket clientSocket = null;
-    private BufferedReader in = null;
-    private PrintWriter out = null;
+  private Socket clientSocket = null;
+  private BufferedReader in = null;
+  private PrintWriter out = null;
 
-    /**
-     * Instantiation of a new worker mapped to a socket
-     *
-     * @param clientSocket connected to worker
-     */
-    public ServerWorker(Socket clientSocket) {
-        // Log output on a single line
-        System.setProperty("java.util.logging.SimpleFormatter.format", "%4$s: %5$s%6$s%n");
+  /**
+   * Instantiation of a new worker mapped to a socket
+   *
+   * @param clientSocket connected to worker
+   */
+  public ServerWorker(Socket clientSocket) {
+    // Log output on a single line
+    System.setProperty("java.util.logging.SimpleFormatter.format", "%4$s: %5$s%6$s%n");
 
-        try {
-            this.clientSocket = clientSocket;
-            in = new BufferedReader(new InputStreamReader(clientSocket.getInputStream()));
-            out = new PrintWriter(clientSocket.getOutputStream());
-        } catch (IOException ex) {
-            Logger.getLogger(ServerWorker.class.getName()).log(Level.SEVERE, null, ex);
-        }
+    try {
+      this.clientSocket = clientSocket;
+      in = new BufferedReader(new InputStreamReader(clientSocket.getInputStream()));
+      out = new PrintWriter(clientSocket.getOutputStream());
+    } catch (IOException ex) {
+      Logger.getLogger(ServerWorker.class.getName()).log(Level.SEVERE, null, ex);
     }
+  }
 
-    /**
-     * Run method of the thread.
-     */
-    @Override
-    public void run() {
-        try
-        {
-            String line;
-            while (!clientSocket.isClosed()) {
-                while ((line = in.readLine()) != null) {
-                    // reads until client close connexion
-                    String[] s = line.split(" ");
-                    if (s.length < 3) {
-                        if (s[0].equalsIgnoreCase("bye"))
-                        {
-                            out.write("Goodbye !\n");
-                            clientSocket.close();
-                            break;
-                        }
-                        sendErrorCode();
-                        continue;
-                    }
-                    float a;
-                    float b;
-                    float answer;
-
-                    try
-                    {
-                        a = Float.parseFloat(s[1]);
-                        b = Float.parseFloat(s[2]);
-                    }
-                    catch (NumberFormatException e)
-                    {
-                        sendErrorCode();
-                        continue;
-                    }
-
-
-                    switch (s[0]) {
-                        case "add":
-                            answer = a + b;
-                            break;
-                        case "sub":
-                            answer = a - b;
-                            break;
-                        case "mul":
-                            answer = a * b;
-                            break;
-                        default:
-                            sendErrorCode();
-                            continue;
-                    }
-                    sendAnswer(answer);
-                }
+  /** Run method of the thread. */
+  @Override
+  public void run() {
+    try {
+      out.write(
+          "Welcome to your online calculator !\n"
+              + "Send 'bye' to close the connection.\n"
+              + "Available commands:\n"
+              + "- add x y\n"
+              + "- sub x y\n"
+              + "- mul x y\nEnjoy !\n\n");
+      out.flush();
+      String line;
+      while (!clientSocket.isClosed()) {
+        while ((line = in.readLine()) != null) {
+          // reads until client close connexion
+          String[] s = line.split(" ");
+          if (s.length < 3) {
+            if (s[0].equalsIgnoreCase("bye")) {
+              out.write("Goodbye !\n");
+              clientSocket.close();
+              break;
             }
-        } catch (IOException e){
-            System.err.println("Client connexion error");
-            System.err.println(e.getMessage());
+            sendErrorCode();
+            continue;
+          }
+          float a;
+          float b;
+          float answer;
+
+          try {
+            a = Float.parseFloat(s[1]);
+            b = Float.parseFloat(s[2]);
+          } catch (NumberFormatException e) {
+            sendErrorCode();
+            continue;
+          }
+
+          switch (s[0]) {
+            case "add":
+              answer = a + b;
+              break;
+            case "sub":
+              answer = a - b;
+              break;
+            case "mul":
+              answer = a * b;
+              break;
+            default:
+              sendErrorCode();
+              continue;
+          }
+          sendAnswer(answer);
         }
+      }
+    } catch (IOException e) {
+      Logger.getLogger(ServerWorker.class.getName()).log(Level.SEVERE, null, e);
+    }
+  }
+
+  private void sendErrorCode() {
+    out.write(Code.ERROR.toString() + "\n");
+    out.flush();
+  }
+
+  private void sendAnswer(float answer) {
+    out.write(Code.OK + " " + answer + "\n");
+    out.flush();
+  }
+
+  private enum Code {
+    OK(0),
+    ERROR(1);
+    private final int value;
+
+    Code(int val) {
+      value = val;
     }
 
-    private void sendErrorCode()
-    {
-        out.write(Code.ERROR.toString());
+    @Override
+    public String toString() {
+      return String.valueOf(value);
     }
-
-    private void sendAnswer(float answer){
-        String string = Code.OK + " " + answer;
-        out.write(string);
-    }
-
-    private enum Code {
-        OK(0),
-        ERROR(1);
-        private final int value;
-
-        Code(int val){
-            value = val;
-        }
-
-        @Override
-        public String toString() {
-            return String.valueOf(value);
-        }
-    }
+  }
 }
