494242dc272172aa6d5217062d9f9ed918f8f667
Eliott <f>
2021-11-04 11:36:28 +0100

    Implement server Lucien

--- ServerMultiThread/src/main/java/ch/heigvd/api/calc/ServerWorker.java
+++ ServerMultiThread/src/main/java/ch/heigvd/api/calc/ServerWorker.java
@@ -13,6 +13,10 @@ public class ServerWorker implements Runnable {
 
     private final static Logger LOG = Logger.getLogger(ServerWorker.class.getName());
 
+    private Socket clientSocket = null;
+    private BufferedReader in = null;
+    private PrintWriter out = null;
+
     /**
      * Instantiation of a new worker mapped to a socket
      *
@@ -22,11 +26,13 @@ public class ServerWorker implements Runnable {
         // Log output on a single line
         System.setProperty("java.util.logging.SimpleFormatter.format", "%4$s: %5$s%6$s%n");
 
-        /* TODO: prepare everything for the ServerWorker to run when the
-         *   server calls the ServerWorker.run method.
-         *   Don't call the ServerWorker.run method here. It has to be called from the Server.
-         */
-
+        try {
+            this.clientSocket = clientSocket;
+            in = new BufferedReader(new InputStreamReader(clientSocket.getInputStream()));
+            out = new PrintWriter(clientSocket.getOutputStream());
+        } catch (IOException ex) {
+            Logger.getLogger(ServerWorker.class.getName()).log(Level.SEVERE, null, ex);
+        }
     }
 
     /**
@@ -34,16 +40,84 @@ public class ServerWorker implements Runnable {
      */
     @Override
     public void run() {
+        try
+        {
+            String line;
+            while (!clientSocket.isClosed()) {
+                while ((line = in.readLine()) != null) {
+                    // reads until client close connexion
+                    String[] s = line.split(" ");
+                    if (s.length < 3) {
+                        if (s[0].equalsIgnoreCase("bye"))
+                        {
+                            out.write("Goodbye !\n");
+                            clientSocket.close();
+                            break;
+                        }
+                        sendErrorCode();
+                        continue;
+                    }
+                    float a;
+                    float b;
+                    float answer;
 
-        /* TODO: implement the handling of a client connection according to the specification.
-         *   The server has to do the following:
-         *   - initialize the dialog according to the specification (for example send the list
-         *     of possible commands)
-         *   - In a loop:
-         *     - Read a message from the input stream (using BufferedReader.readLine)
-         *     - Handle the message
-         *     - Send to result to the client
-         */
+                    try
+                    {
+                        a = Float.parseFloat(s[1]);
+                        b = Float.parseFloat(s[2]);
+                    }
+                    catch (NumberFormatException e)
+                    {
+                        sendErrorCode();
+                        continue;
+                    }
 
+
+                    switch (s[0]) {
+                        case "add":
+                            answer = a + b;
+                            break;
+                        case "sub":
+                            answer = a - b;
+                            break;
+                        case "mul":
+                            answer = a * b;
+                            break;
+                        default:
+                            sendErrorCode();
+                            continue;
+                    }
+                    sendAnswer(answer);
+                }
+            }
+        } catch (IOException e){
+            System.err.println("Client connexion error");
+            System.err.println(e.getMessage());
+        }
+    }
+
+    private void sendErrorCode()
+    {
+        out.write(Code.ERROR.toString());
+    }
+
+    private void sendAnswer(float answer){
+        String string = Code.OK + " " + answer;
+        out.write(string);
+    }
+
+    private enum Code {
+        OK(0),
+        ERROR(1);
+        private final int value;
+
+        Code(int val){
+            value = val;
+        }
+
+        @Override
+        public String toString() {
+            return String.valueOf(value);
+        }
     }
 }
