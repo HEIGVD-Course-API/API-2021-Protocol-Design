93d175dfa90809ea2b33ad7f2c5a737dc0c49ef7
Jonathan <jonathan.stocchetti@heig-vd.ch>
2021-11-08 16:47:47 +0100

    Strarting Client-Side

--- Client/src/main/java/ch/heigvd/api/calc/Client.java
+++ Client/src/main/java/ch/heigvd/api/calc/Client.java
@@ -1,8 +1,11 @@
 package ch.heigvd.api.calc;
 
 import java.io.BufferedReader;
+import java.io.IOException;
 import java.io.InputStreamReader;
+import java.io.PrintWriter;
 import java.util.logging.Level;
+import java.net.Socket;
 import java.util.logging.Logger;
 
 /**
@@ -11,18 +14,28 @@ import java.util.logging.Logger;
 public class Client {
 
     private static final Logger LOG = Logger.getLogger(Client.class.getName());
+    private final static int PORT = 42000;
+    private final static String IP = "127.0.0.1";
 
     /**
      * Main function to run client
      *
      * @param args no args required
      */
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException {
         // Log output on a single line
         System.setProperty("java.util.logging.SimpleFormatter.format", "%4$s: %5$s%6$s%n");
-
         BufferedReader stdin = null;
 
+        final Socket CLIENT_SOCKET;
+        final PrintWriter writer;
+        Client client = new Client();
+
+
+        CLIENT_SOCKET = new Socket(IP,PORT);
+        
+
+        
         /* TODO: Implement the client here, according to your specification
          *   The client has to do the following:
          *   - connect to the server
@@ -35,5 +48,12 @@ public class Client {
 
         stdin = new BufferedReader(new InputStreamReader(System.in));
 
+    }
+
+    private static void logSocketAddress(Socket clientSocket) {
+        LOG.log(Level.INFO, "       Local IP address: {0}", new Object[]{clientSocket.getLocalAddress()});
+        LOG.log(Level.INFO, "             Local port: {0}", new Object[]{Integer.toString(clientSocket.getLocalPort())});
+        LOG.log(Level.INFO, "  Remote Socket address: {0}", new Object[]{clientSocket.getRemoteSocketAddress()});
+        LOG.log(Level.INFO, "            Remote port: {0}", new Object[]{Integer.toString(clientSocket.getPort())});
     }
 }
