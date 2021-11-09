c3174b0e9e80eaee73c55d691295cefafad27d2e
Hugo Huart <hugo.huart@heig-vd.ch>
2021-11-09 09:59:49 +0100

    Commands working

--- Client/src/main/java/ch/heigvd/api/calc/Client.java
+++ Client/src/main/java/ch/heigvd/api/calc/Client.java
@@ -3,6 +3,7 @@ package ch.heigvd.api.calc;
 import java.io.*;
 import java.net.Socket;
 import java.net.SocketException;
+import java.util.Locale;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 
@@ -13,7 +14,9 @@ public class Client {
 
     private static final Logger LOG = Logger.getLogger(Client.class.getName());
 
+    private static final String host = "localhost";
     private static final int port = 11111;
+
     private static Socket socket;
     private static BufferedReader in = null;
     private static BufferedWriter out = null;
@@ -41,24 +44,58 @@ public class Client {
 
         stdin = new BufferedReader(new InputStreamReader(System.in));
         try {
-            socket = new Socket("localhost", port);
+            try {
+                socket = new Socket(host, port);
+            } catch (SocketException e) {
+                System.out.printf("Can't connect to server '%s' on port %d\n", host, port);
+                return;
+            }
+
             in = new BufferedReader(new InputStreamReader(socket.getInputStream()));
             out = new BufferedWriter(new OutputStreamWriter(socket.getOutputStream()));
 
-            System.out.println("Salut je suis le client");
+            // This should display the first 'ACK' message
+            System.out.printf("Connected to server '%s' on port %d\n", host, port);
+            System.out.println(in.readLine());
+
             String q = "";
             String r = "";
             while (true) {
+                System.out.print("$: ");
                 q = stdin.readLine();
-                System.out.printf("Je vais envoyer: '%s'\n", q);
-                out.write(q + "\r\n");
-                out.flush();
+                if (!q.equals("")) {
+                    q = q.toLowerCase(Locale.ROOT);
+                    out.write(q + "\r\n");
+                    out.flush();
 
-                r = in.readLine();
-                System.out.println(r);
+                    r = in.readLine();
+                    System.out.println(r);
+
+                    // We may have requested the end of the connection with 'exit'
+                    if (q.equals("exit"))
+                        break;
+                }
             }
         } catch (IOException e) {
             LOG.log(Level.SEVERE, null, e);
+        } finally {
+            // Closing the connections properly
+            try {
+                if (!socket.isClosed())
+                    socket.close();
+            } catch (IOException e) {
+                LOG.log(Level.SEVERE, null, e);
+            }
+            try {
+                in.close();
+            } catch (IOException e) {
+                LOG.log(Level.SEVERE, null, e);
+            }
+            try {
+                out.close();
+            } catch (IOException e) {
+                LOG.log(Level.SEVERE, null, e);
+            }
         }
     }
 }
--- ServerMultiThread/src/main/java/ch/heigvd/api/calc/ServerWorker.java
+++ ServerMultiThread/src/main/java/ch/heigvd/api/calc/ServerWorker.java
@@ -46,31 +46,98 @@ public class ServerWorker implements Runnable {
     @Override
     public void run() {
 
-        /* TODO: implement the handling of a client connection according to the specification.
-         *   The server has to do the following:
-         *   - initialize the dialog according to the specification (for example send the list
-         *     of possible commands)
-         *   - In a loop:
-         *     - Read a message from the input stream (using BufferedReader.readLine)
-         *     - Handle the message
-         *     - Send to result to the client
-         */
         try {
-            System.out.println("Coucou je suis le run du thread");
-            String s = "";
-            String[] content = new String[]{};
-            int i = 0;
+            out.write("[ACK] : Welcome!\r\n");
+            out.flush();
+
+            String query;
+            String response;
+            String[] content;
+
+            // Represents the conversation's state:
+            // 0 -> 2 : operand (in)validity
+            // -1 : error while performing the computation
+            // -2 : exiting
+            short state;
+
+            int lhs = 0;
+            int rhs = 0;
+            int result = 0;
             while (!clientSocket.isClosed()) {
-                System.out.println(++i);
-                s = in.readLine();
-                if (s == null)
+
+                query = in.readLine();
+                if (query == null)
                     break;
-                System.out.println(s);
-                content = s.split(" +");
-                if (content.length != 0 && content.length != 3) {
-                    System.out.println("[ERROR]");
-                    out.write("[ERROR] : Wrong expression\r\n");
-                    out.flush();
+
+                System.out.println(query);
+                content = query.split(" +");
+
+                System.out.printf("content's content [%d]:\n", content.length);
+                for (int i = 0; i < content.length; ++i)
+                    System.out.printf("'%s' ", content[i]);
+                System.out.println();
+
+                response = "";
+                state = 0;
+                if (content.length != 3) {
+                    if (content.length == 1 && content[0].equals("exit")) {
+                        response = "[ACK] : Goodbye!";
+                        state = -2;
+                    } else {
+                        response = "[ERROR] : Bad expression, expected 'operator int1 int2'";
+                    }
+                } else {
+                    try {
+                        lhs = Integer.parseInt(content[1]);
+                        ++state;
+                    } catch (NumberFormatException e) {
+                        response = "[ERROR] : Left operand is not a valid integer";
+                    }
+
+                    try {
+                        rhs = Integer.parseInt(content[2]);
+                        ++state;
+                    } catch (NumberFormatException e) {
+                        if (state == 1)
+                            response = "[ERROR] : Right operand is not a valid integer";
+                        else // ok == 0, meaning we were not able to read both operands
+                            response = "[ERROR] : Both operands are not valid integers";
+                    }
+
+                    if (state == 2) {
+                        switch (content[0]) {
+                            case "+":
+                                result = lhs + rhs;
+                                break;
+                            case "-":
+                                result = lhs - rhs;
+                                break;
+                            case "*":
+                                result = lhs * rhs;
+                                break;
+                            case "/":
+                                if (rhs == 0) {
+                                    response = "[ERROR] : Division by zero";
+                                    state = -1;
+                                } else {
+                                    result = lhs / rhs;
+                                }
+                                break;
+                            default:
+                                response = String.format("[ERROR] : Unknown operator '%s'", content[0]);
+                                state = -1;
+                                break;
+                        }
+                        if (state > -1)
+                            response = String.format("[OK] : %d", result);
+                    }
+                }
+
+                out.write(response + "\r\n");
+                out.flush();
+                if (state == -2) {
+                    clientSocket.close();
+                    break;
                 }
             }
         } catch (IOException e) {
@@ -78,7 +145,8 @@ public class ServerWorker implements Runnable {
         } finally {
             // Closing the connections properly
             try {
-                clientSocket.close();
+                if (!clientSocket.isClosed())
+                    clientSocket.close();
             } catch (IOException e) {
                 LOG.log(Level.SEVERE, null, e);
             }
@@ -92,7 +160,6 @@ public class ServerWorker implements Runnable {
             } catch (IOException e) {
                 LOG.log(Level.SEVERE, null, e);
             }
-            System.out.println("Le thread dit bye-bye");
         }
 
     }
