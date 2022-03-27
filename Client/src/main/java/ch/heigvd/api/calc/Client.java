1e18563f99d6adb4e6bc5f92914ec4801640f567
YanickHEIG <yanick.thomann@heig-vd.ch>
2022-03-27 15:29:28 +0200

    First client version

--- Client/src/main/java/ch/heigvd/api/calc/Client.java
+++ Client/src/main/java/ch/heigvd/api/calc/Client.java
@@ -1,7 +1,8 @@
 package ch.heigvd.api.calc;
 
-import java.io.BufferedReader;
-import java.io.InputStreamReader;
+import java.io.*;
+import java.net.Socket;
+import java.net.UnknownHostException;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 
@@ -32,8 +33,56 @@ public class Client {
          *     - send the command to the server
          *     - read the response line from the server (using BufferedReader.readLine)
          */
+        /* Check args have been given */
+        if (args.length != 2){
+            System.out.println("Invalid number of arguments specified.");
+            return;
+        }
 
-        stdin = new BufferedReader(new InputStreamReader(System.in));
+        String serverAddress = args[0];
+        int serverPort = Integer.valueOf(args[1]);
+
+        OutputStream socketOutputStream = null;
+        InputStream socketInputStream = null;
+        String welcome, command, result;
+
+        try {
+            Socket socket = new Socket(serverAddress, serverPort);
+            socketInputStream = socket.getInputStream();
+            socketOutputStream = socket.getOutputStream();
+
+            BufferedReader socketReader = new BufferedReader(new InputStreamReader(socketInputStream));
+
+            // Read welcome message
+            welcome = socketReader.readLine();
+            System.out.println(welcome);
+
+
+            while(!socket.isClosed()){
+                // Read command prompt
+                command = socketReader.readLine();
+                System.out.println(command);
+
+                // Read input
+                stdin = new BufferedReader(new InputStreamReader(System.in));
+
+                // Send user input to server
+                socketOutputStream.write(stdin.read());
+                socketOutputStream.flush();
+                // Read response from server
+                result = socketReader.readLine();
+                System.out.println(result);
+
+            }
+        } catch (UnknownHostException e){
+            System.out.println("Cannot establish connection to host " + serverAddress + " on port " + serverPort);
+            return;
+        } catch (IOException e){
+            System.out.println(e);
+            return;
+        }
+
+
 
     }
 }
